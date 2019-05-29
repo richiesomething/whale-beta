9d389b8b02adc38f30c008552ff731e67ad6f2bb
Chris McKenzie <kristopolous@yahoo.com>
2019-05-29 14:14:57 -0700

    Restructuring the server file and bringing in cors and login

--- server.py
+++ server.py
@@ -1,69 +1,105 @@
 #!/usr/bin/python3
 
 import flask
+from flask_cors import CORS
+from stocks import robin
 
 import hp
 import whale
 
 
-if __name__ == "__main__":
-    def main():
-        app = flask.Flask(__name__)
-        hp_server = hp.Server()
-        whale_game = hp.Game("whale", whale.Room)
-        assert hp_server.add_game(app, whale_game)
+app = flask.Flask(__name__)
+app.secret_key = 'hT0yRAvrQcGbKbJkVE3kAw'
+CORS(app)
 
-        #
-        # Assets:
-        #
+hp_server = hp.Server()
+whale_game = hp.Game("whale", whale.Room)
+login_manager = LoginManager()
 
-        @app.route("/")
-        def index():
-            return flask.render_template("index.html")
+assert hp_server.add_game(app, whale_game)
 
-        @app.route("/whale")
-        def whale_page():
-            game_id = whale_game.id
-            room_id = whale_game.add_room()
-            player_id = whale_game.add_player(room_id)
-            return flask.render_template("whale.html", game_id=game_id, room_id=room_id, player_id=player_id)
 
-        @app.route("/js/<path:sub_path>")
-        def js(sub_path):
-            return flask.send_from_directory("js/", sub_path)
+# Standard wrappers for api calls
+def res(what):
+    return jsonify(what)
 
-        @app.route("/css/<path:sub_path>")
-        def css(sub_path):
-            return flask.send_from_directory("css/", sub_path)
+def success(what):
+    return res({'res': True, 'data': what})
 
-        @app.route("/img/<res>/<path:name>")
-        def img(res, name):
-            if res == "svg":
-                return flask.send_from_directory("img/svg", f"{name}.svg")
-            elif res == "x1":
-                return flask.send_from_directory("img/x1", f"{name}.png")
-            elif res == "x2":
-                return flask.send_from_directory("img/x2", f"{name}.png")
-            else:
-                # Default to 3x resolution for unknown 'res' strings:
-                return flask.send_from_directory("img/x3", f"{name}.png")
+def failure(what):
+    return res({'res': False, 'data': what})
+# }} // wrappers
 
-        @app.route("/font/<path:sub_path>")
-        def font(sub_path):
-            return flask.send_from_directory("font", sub_path)
 
-        @app.route("/audio/<path:sub_path>")
-        def audio(sub_path):
-            return flask.send_from_directory("audio", sub_path + ".ogg")
+# Static {{
+# TODO: nginx should be doing the static stuff. This is stupid
+@app.route("/js/<path:sub_path>")
+def js(sub_path):
+    return flask.send_from_directory("js/", sub_path)
 
-        #
-        # Game (can be moved to a different server for load-balancing!
-        #
+@app.route("/css/<path:sub_path>")
+def css(sub_path):
+    return flask.send_from_directory("css/", sub_path)
 
-        # app.run(debug=False, host="0.0.0.0")
-        app.run()
+@app.route("/img/<res>/<path:name>")
+def img(res, name):
+    if res == "svg":
+        return flask.send_from_directory("img/svg", f"{name}.svg")
+    elif res == "x1":
+        return flask.send_from_directory("img/x1", f"{name}.png")
+    elif res == "x2":
+        return flask.send_from_directory("img/x2", f"{name}.png")
+    else:
+        # Default to 3x resolution for unknown 'res' strings:
+        return flask.send_from_directory("img/x3", f"{name}.png")
 
-    main()
+@app.route("/font/<path:sub_path>")
+def font(sub_path):
+    return flask.send_from_directory("font", sub_path)
+
+@app.route("/audio/<path:sub_path>")
+def audio(sub_path):
+    return flask.send_from_directory("audio", sub_path + ".ogg")
+
+# }} //Static
+
+# Templates {{
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+@app.route("/whale")
+def whale_page():
+    game_id = whale_game.id
+    room_id = whale_game.add_room()
+    player_id = whale_game.add_player(room_id)
+    return flask.render_template("whale.html", game_id=game_id, room_id=room_id, player_id=player_id)
+
+# }} //Templates
+
+# Stock Stuff {{
+@app.route('/yesterday')
+def yesterday():
+    return success(robin.get_yesterday('ticker, open, close'))
+
+# }} //Stock Stuff
+
+@app.route("/register", methods=['PUT', 'POST']))
+def register():
+  pass
+
+@app.route("/login", methods=['PUT', 'POST']))
+def login():
+  pass
+
+@app.route("/logout", methods=['PUT', 'POST']))
+def logout():
+  pass
+
+login_manager.init_app(app)
+# app.run(debug=False, host="0.0.0.0")
+app.run()
+
 
 
 # TODO:
