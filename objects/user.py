4e93ffb34ccb0ede33a543660b288919bcccd5c4
Chris McKenzie <kristopolous@yahoo.com>
2019-05-29 15:24:53 -0700

    Fleshing out the basic user class

--- objects/user.py
+++ objects/user.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
 from . import db
+import flask_login
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User:
-  is_authenticated = False
-  is_active = False
-  is_anonymous = False
-  def get_id():
+class User(BaseClass, flask_login.mixins):
+  def __init__(self, email = None):
+    self.email = email
 
+  def _load(self, attrib):
+    for k,v in attrib.items():
+      setattr(self, k, v)
+
+  @staticmethod
+  def get(self, user_id):
+    res = db.get('users', user_id)
+    if res:
+      return User()._load(res)
+
+
+  def set_password(what):
+    hash = generate_password_hash(what)
